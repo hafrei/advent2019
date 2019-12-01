2f32616a6f5c645111cf0ef95d71a490d16fdc9a
hafrei <h.a.freiburger@gmail.com>
2019-12-01 16:11:34 -0500

    Finished part 2

--- day1/challenge1.py
+++ day1/challenge1.py
@@ -3,12 +3,19 @@ from functools import reduce
 
 result_set = []
 
+def get_fuel(x):
+  res = int(floor(x/3))-2
+  if res <= 0:
+    return
+  else:
+    result_set.append(res)
+    get_fuel(res)
+
 with open('input1.txt') as f:
   blargle = f.read().splitlines()
 
 for x in blargle:
-  res = floor(int(x)/3)-2
-  result_set.append(res)
+  get_fuel(int(x))
 
 fuel_required = reduce((lambda x, y: x + y), result_set)
 
