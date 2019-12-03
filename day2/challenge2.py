424cdfaa9469eb96d40f4896d9f583db8058f287
hafrei <h.a.freiburger@gmail.com>
2019-12-02 20:50:32 -0500

    Solved day 2 part 2

--- day2/challenge2.py
+++ day2/challenge2.py
@@ -14,38 +14,60 @@ def stop_code(ops):
     assume_false = True
   return assume_false
 
-def process_vars(ops,x,y,sav,arr):
+def process_vars(ops,x,y,sav,arr,log_out):
   if ops == 1:
-    print("Performing add on {} and {}. Saving at {}".format(arr[x], arr[y], sav))
+    if log_out:
+      print("Performing add on {} and {}. Saving at {}".format(arr[x], arr[y], sav))
     store(add(arr[x],arr[y]),sav,arr)
   if ops == 2:
-    print("Performing multi on {} and {}. Saving at {}".format(arr[x], arr[y], sav))
+    if log_out:
+      print("Performing multi on {} and {}. Saving at {}".format(arr[x], arr[y], sav))
     store(multi(arr[x],arr[y]),sav,arr)
 
+def runner(gravity_assist,log_out):
+  is_stop = False
+  opcode_pos = 0
+  x_pos = 1
+  y_pos = 2
+  sav_pos = 3
+  args = 4
+  day2_target = 19690720
+
+  while is_stop == False:
+    temp_ops = gravity_assist[opcode_pos]
+    temp_x = gravity_assist[x_pos]
+    temp_y = gravity_assist[y_pos]
+    temp_sav = gravity_assist[sav_pos]
+    is_stop = stop_code(temp_ops)
+    if is_stop:
+      if gravity_assist[0] == day2_target:
+        print("Encountered ops code 99. Terminating with {}, noun {} and verb {}".format(gravity_assist[0],gravity_assist[1],gravity_assist[2]))
+      if log_out:
+        print("Encountered ops code 99. Terminating with {}, noun {} and verb {}".format(gravity_assist[0],gravity_assist[1],gravity_assist[2]))
+      break
+    else:
+      if log_out:
+        print("Working on ops {} with {} and {} locations, saving at {}".format(temp_ops, temp_x, temp_y, temp_sav))
+      process_vars(temp_ops,temp_x, temp_y,temp_sav, gravity_assist, log_out)
+      opcode_pos += args
+      x_pos += args
+      y_pos += args
+      sav_pos += args
+
 gravity_assist = []
-is_stop = False
-opcode_pos = 0
-x_pos = 1
-y_pos = 2
-sav_pos = 3
+reset_gravity_assist = []
+log_out = False
 
 with open('input1.txt') as f:
-  gravity_assist = list(map(int, f.read().split(',')))
+  reset_gravity_assist = list(map(int, f.read().split(',')))
   print("Input list loaded.")
 
-while is_stop == False:
-  temp_ops = gravity_assist[opcode_pos]
-  temp_x = gravity_assist[x_pos]
-  temp_y = gravity_assist[y_pos]
-  temp_sav = gravity_assist[sav_pos]
-  is_stop = stop_code(temp_ops)
-  if is_stop:
-    print("Encountered ops code 99. Terminating with {}".format(gravity_assist[0]))
-    break
-  else:
-    print("Working on ops {} with {} and {} locations, saving at {}".format(temp_ops, temp_x, temp_y, temp_sav))
-    process_vars(temp_ops,temp_x, temp_y,temp_sav, gravity_assist)
-    opcode_pos += 4
-    x_pos += 4
-    y_pos += 4
-    sav_pos += 4
+for bfy in range(100):
+  for bfx in range(100):
+    gravity_assist = reset_gravity_assist.copy()
+    gravity_assist[1] = int(bfy)
+    gravity_assist[2] = int(bfx)
+    try:
+      runner(gravity_assist,log_out)
+    except:
+      print("Blew up on {},{}. Restarting...".format(bfx, bfy))
